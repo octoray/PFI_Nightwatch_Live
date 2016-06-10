3d21cf0c064314e78d6769573f0c89fdd0d7ccbc
octoray <david.wintour@gmail.com>
2016-06-10 10:54:29 +0100

    change search from fail to failed

--- run_alerting/run_fail_check_script.sh
+++ run_alerting/run_fail_check_script.sh
@@ -1,4 +1,4 @@
-if resultfilenames=$(grep -il "fail" /home/nightwatch/node_modules/nightwatch/run_reports/Tekka/*.xml); then
+if resultfilenames=$(grep -il "failed" /home/nightwatch/node_modules/nightwatch/run_reports/Tekka/*.xml); then
    echo "Errors/Changes were found !!"
    echo $resultfilenames
    php php_email_alert_script.php $resultfilenames
